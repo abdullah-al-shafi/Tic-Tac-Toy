b1cbe2778a6625383a69e827c873003b828d176d
Abdullah Al Shafi <abdullah_al.shafi@outlook.com>
2019-05-07 08:27:45 +0600

    Button Click done

--- app/src/main/java/shafi/sbf/com/tictactoy/MainActivity.kt
+++ app/src/main/java/shafi/sbf/com/tictactoy/MainActivity.kt
@@ -2,11 +2,32 @@ package shafi.sbf.com.tictactoy
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+
+    protected fun buClick(view: View) {
+        val buSelected = view as Button
+        var cellId = 0
+        when(buSelected.id){
+            R.id.bu1 -> cellId = 1
+            R.id.bu2 -> cellId = 2
+            R.id.bu3 -> cellId = 3
+            R.id.bu4 -> cellId = 4
+            R.id.bu5 -> cellId = 5
+            R.id.bu6 -> cellId = 6
+            R.id.bu7 -> cellId = 7
+            R.id.bu8 -> cellId = 8
+            R.id.bu9 -> cellId = 9
+        }
+        Toast.makeText(this,"ID: "+cellId, Toast.LENGTH_SHORT).show()
     }
 }
